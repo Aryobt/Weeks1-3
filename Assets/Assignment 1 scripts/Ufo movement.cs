8971de6ef329b8a6da6e009a6af8f956b983fc11
Aryobarzan Taghavi <taghavia@shernet.sheridancollege.ca>
2025-01-27 16:04:41 -0500

    made the moon spin around the eart

--- Assets/Assignment 1 scripts/Ufo movement.cs
+++ Assets/Assignment 1 scripts/Ufo movement.cs
@@ -7,9 +7,11 @@ public class Ufomovement : MonoBehaviour
 {
     [Range(0, 1)]
     public float c;
+    public float Ufospeed;
     public AnimationCurve curve;
     public Transform begin;
     public Transform end;
+    bool start = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +21,18 @@ public class Ufomovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Vector2 pos = transform.position;
+        pos.x += Ufospeed * Time.deltaTime;
+
+        Vector2 begin = Camera.main.WorldToScreenPoint(pos);
+
+        if (begin.x < 0 || begin.x > end)
         {
-            transform.position = Vector2.Lerp(begin.position, end.position, curve.Evaluate(c));
+            Ufospeed = Ufospeed * -1;
         }
+
+        transform.position = pos;
     }
+
+
 }
