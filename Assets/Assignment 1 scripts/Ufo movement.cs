46f05a1f6c045ed060961438f70781ae5fe708cd
Aryobarzan Taghavi <taghavia@shernet.sheridancollege.ca>
2025-01-27 17:22:04 -0500

    fixing issue with Ufo movement

--- Assets/Assignment 1 scripts/Ufo movement.cs
+++ Assets/Assignment 1 scripts/Ufo movement.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class Ufomovement : MonoBehaviour
 {
- 
+    [Range(0, 1)]
+    public float c;
+    public AnimationCurve curve;
+    public Transform begin;
+    public Transform end;
+    public float Ufospeed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +20,12 @@ public class Ufomovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = Vector2.Lerp(begin.position, end.position, curve.Evaluate(c));
 
+        if((Input.GetKeyUp(KeyCode.Space)
+            {
+            transform.position += transform. * Ufospeed * Time.deltaTime;
+        }
     }
 
 
