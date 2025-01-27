46e161cf55b9aac5f44e2a333a2317ea8017aeb5
Aryobarzan Taghavi <taghavia@shernet.sheridancollege.ca>
2025-01-27 16:59:10 -0500

    figuring out the issue with the ufo movement

--- Assets/Assignment 1 scripts/Ufo movement.cs
+++ Assets/Assignment 1 scripts/Ufo movement.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 
 public class Ufomovement : MonoBehaviour
 {
-    [Range(0, 1)]
-    public float c;
-    public float Ufospeed;
-    public AnimationCurve curve;
-    public Transform begin;
-    public Transform end;
-    bool start = false;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +15,7 @@ public class Ufomovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
-        pos.x += Ufospeed * Time.deltaTime;
 
-        Vector2 begin = Camera.main.WorldToScreenPoint(pos);
-
-        if (begin.x < 0 || begin.x > end)
-        {
-            Ufospeed = Ufospeed * -1;
-        }
-
-        transform.position = pos;
     }
 
 
