7242dfdbfc8c48a200892aeec880546140ec1f11
Aryobarzan Taghavi <taghavia@shernet.sheridancollege.ca>
2025-01-27 13:11:09 -0500

    Made code for Ufo mostly done

--- Assets/Assignment 1 scripts/Ufo movement.cs
+++ Assets/Assignment 1 scripts/Ufo movement.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ufomovement : MonoBehaviour
 {
+    [Range(0, 1)]
+    public float c;
+    public AnimationCurve curve;
+    public Transform begin;
+    public Transform end;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,9 @@ public class Ufomovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = Vector2.Lerp(begin.position, end.position, curve.Evaluate(c));
+        }
     }
 }
